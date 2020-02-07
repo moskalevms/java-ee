33232b0b95e5c4885864f9b3f7b8b22d0a718bbf
moskalevms <mis-van@yandex.ru>
2020-02-07 22:55:08 +0300

    ДЗ по шестому уроку

--- base-jsf-app/src/main/java/ru/moskalevms/persist/ProductDAO.java
+++ base-jsf-app/src/main/java/ru/moskalevms/persist/ProductDAO.java
@@ -1,34 +1,43 @@
 package ru.moskalevms.persist;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import java.util.List;
+import javax.ejb.TransactionAttribute;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 
 @Stateless
-public class ProductDAO implements ProductRepository {
+public class ProductDAO {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProductDAO.class);
+
+    @PersistenceContext(unitName = "ds")
+    private EntityManager em;
+
+    @EJB
+    private ProductRepositoryImpl productRepositoryImpl;
 
 
-    @Override
-    public void insert(Product product) {
-
+    @TransactionAttribute
+    public void insert(Product product)  {
+        em.persist(product);
     }
 
-    @Override
+    @TransactionAttribute
     public void update(Product product) {
-
+        em.merge(product);
+    }
+  
+    @TransactionAttribute
+    public void delete(long id)  {
+        Product product = em.find(Product.class, id);
+        if(product != null) {
+            em.remove(product);
+        }
     }
 
-    @Override
-    public void delete(long id) {
-
-    }
-
-    @Override
-    public Product findById(long id) {
-        return null;
-    }
-
-    @Override
-    public List<Product> findAll() {
-        return null;
-    }
 }
